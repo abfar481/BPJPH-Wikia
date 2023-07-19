56c5035ddfc7f47a3e5a6ed5f0b5beca9a74b716
abfar481 <abifarabi4881@gmail.com>
2023-07-19 20:09:50 +0700

    fixed data bugs

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,28 +1,39 @@
+// src/Article.js
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import articles from '../data/articles.json'
 
-const Article = ({ article }) => {
+const Article = () => {
 
-  const { id, title, content, author } = article
+  const { id } = useParams()
+
+  const article = articles.find((article) => article.id === parseInt(id, 10))
+
+  if (!article) {
+
+    return <div> Article not found. </div>
+  }
+
+  const { title, content, author } = article
 
   return (
-    <>
-      <Link to={`/article/${id}`}>
-        <div className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md mb-4">
-          <h2 className="text-xl font-bold mb-2">
-            {title}
-          </h2>
-          <p className="text-gray-600 mb-1">
-            {`Author: ${author}`}
-          </p>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl p-8 bg-white shadow-md rounded-md">
+        <h2 className="text-3xl font-bold mb-4">
+          {title}
+        </h2>
+        <p className="text-gray-600 mb-2">
+          {`Author: ${author}`}
+        </p>
+        <div className="border-t border-gray-300 pt-4">
           <p className="text-gray-800">
-            {`${content.substring(0, 100)}...`}
+            {content}
           </p>
         </div>
-      </Link>
-    </>
+      </div>
+    </div>
   )
 }
 
-export default Article;
+export default Article
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { Article } from '../components'
 import articles from '../data/articles.json'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
@@ -12,11 +13,32 @@ const HomePage = () => {
             Dictionary
           </h1>
           {articles.map((article) => (
-            <Article key={article.id} article={article} />
+            <ArticleCard key={article.id} article={article} />
           ))}
         </div>
       </div>
     </>
+  )
+}
+
+const ArticleCard = ({ article }) => {
+
+  const { id, title, content, author } = article
+
+  return (
+    <Link to={`/article/${id}`}>
+      <div className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md mb-4">
+        <h2 className="text-xl font-bold mb-2">
+          {title}
+        </h2>
+        <p className="text-gray-600 mb-1">
+          {`Author: ${author}`}
+        </p>
+        <p className="text-gray-800">
+          {`${content.substring(0, 100)}...`}
+        </p>
+      </div>
+    </Link>
   )
 }
 
